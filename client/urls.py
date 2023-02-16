51b460df13deffee4cb5dc62c85045cae79dd6d5
KOUADIO KOUASSI ROMARIC <106255547+KOUASSI-Romaric@users.noreply.github.com>
2023-02-16 02:08:15 +0000

    GRUB fonction for client

--- client/urls.py
+++ client/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 
     path('/<str:pk>', views.list_client, name='client',),  # la vue qui sera activée lorsque je suis dans l'accueil
+    path('/', views.ajouter_client, name='ajouter_client',),
+    path('/<str:pk>', views.modifier_client, name='modifier_client',),
+    path('/<str:pk>', views.supprimer_client, name='supprimer_client',),
 ]
--- client/views.py
+++ client/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Client
+from .forms import ClientForm
 
 
 # Create your views here.
@@ -10,3 +10,27 @@ def list_client(request, pk):
     commande_total = commande.count()
     context = {'client': client, 'commande': commande, 'commande_total': commande_total}
     return render(request, 'client/list_client.html', context)
+
+
+def ajouter_client(request):
+    form = ClientForm()
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'client/ajouter_client.html', context)
+
+
+def modifier_client(request, pk):
+    client = Client.objects.get(id=pk)
+
+    return render(request, 'client/modifier_client.html')
+
+
+def supprimer_client(request, pk):
+    client = Client.objects.get(id=pk)
+
+    return render(request, 'client/supprimer_client.html')
